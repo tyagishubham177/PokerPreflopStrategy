066b6258361cf88cf9a7936ef8d3e80ff3fa2677
shubham tyagi <shubham.tyagi@soprasteria.com>
2024-07-07 11:58:54 +0530

    data added

--- src/Constants/InitialStrategy.js
+++ src/Constants/InitialStrategy.js
@@ -106,11 +106,36 @@ export const initialPokerStrategy = {
         "32s"
       ],
     },
+    "Small Blind": {
+      "Raise for Value": [
+        "AKs", "AQs", "AJs", "ATs", "AQo", "AJo", "ATo",
+        "KQs", "KJs", "KQo", "KJo",
+        "QQ", "JJ", "TT", "99", "88"
+      ],
+      "Raise as a bluff": [
+        "K3o", "K2o", "Q5o", "Q4o", "Q3o", "Q2o",
+        "J4s", "J3s", "J2s", "T5s", "T4s", "95s", "94s", "85s", "84s", "74s", "63s", "53s", "43s",
+        "J6o", "T6o", "96o", "86o"
+      ],
+      "Limp LR": [
+        "AA", "KK", "77", "66", "55", "44", "33", "22",
+        "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
+        "A9o", "A8o", "A7o", "A6o", "A5o", "A4o", "A3o", "A2o",
+        "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s",
+        "KTo", "K9o", "K8o", "K7o", "K6o", "K5o", "K4o",
+        "QTs", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s",
+        "QJo", "QTo", "Q9o", "Q8o", "Q7o", "Q6o",
+        "JTs", "J9s", "J8s", "J7s", "J6s", "J5s",
+        "JTo", "J9o", "J8o", "J7o",
+        "T9s", "T8s", "T8s", "T6s", "98s", "97s", "96s", "87s", "86s", "76s", "75s", "65s", "64s", "54s", "32s",
+        "T9o", "T8o", "T7o", "98o", "97o", "87o", "76o", "65o"
+      ]
+    }
   },
   "Facing RFI EP/MP": {
     "UTG+1 vs UTG": {
       "3-bet for value": ["AA", "AKs", "AKo", "KK", "QQ"],
-      "3-bet as a bluff": ["AQo", "ATs", "KJs"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs"],
       Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "99", "88"],
     },
     "UTG+2 vs UTG/UTG+1": {
@@ -123,5 +148,30 @@ export const initialPokerStrategy = {
       "3-bet as a bluff": ["AQo", "ATs", "KJs", "A5s"],
       Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "88", "77"],
     },
+    "LJ vs UTG+2": {
+      "3-bet for value": ["AA", "AKs", "AKo", "KK", "QQ"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs", "A5s"],
+      Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "88", "77"],
+    },
+    "HJ vs UTG": {
+      "3-bet for value": ["AA", "AKs", "AKo", "KK", "QQ"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs", "A5s"],
+      Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "88", "77"],
+    },
+    "HJ vs UTG+1": {
+      "3-bet for value": ["AA", "AKs", "AKo", "KK", "QQ"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs", "A5s"],
+      Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "88", "77", "66"],
+    },
+    "HJ vs UTG+2": {
+      "3-bet for value": ["AA", "AKs", "AKo", "KK", "QQ"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs", "A5s", "A4s", "A3s", "87s", "76s"],
+      Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "98s", "88", "77", "66", "55"],
+    },
+    "HJ vs LJ": {
+      "3-bet for value": ["AA", "AKs", "AQs", "AJs", "AKo", "AQo", "KK", "QQ", "KQs"],
+      "3-bet as bluff": ["AQo", "ATs", "KJs", "A5s", "A4s", "A3s", "A2s", "AJo", "ATo", "KQo", "76s", "65s"],
+      Call: ["AQs", "AJs", "KQs", "QJs", "JJ", "JTs", "TT", "T9s", "99", "98s", "88", "77", "66", "55", "87s"],
+    }
   },
 };
